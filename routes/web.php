9b961fad2bd0093605e4c96195e9cf3d010722a0
Rizqi Nur Andi Putra <e31211961@student.polije.ac.id>
2024-09-05 14:40:52 +0700

    perubahan

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\ProductEntryDetailController;
 //Auth Routes
 Route::get('/', [AuthController::class, 'index'])->name('login');
 Route::post('/masuk', [AuthController::class, 'login'])->name('login.post');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
 Route::middleware(['isLogin'])->group(function () {
